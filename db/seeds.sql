3b1a01174081d8e80a8d87521ecbe7ddd7fc1ec3
Mike Worthen <mikeworthen55@gmail.com>
2019-08-19 23:20:37 -0600

    initial commit/ all is working.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Flying Dutchman', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Mac-Attack', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Pork-Belly King', false);
